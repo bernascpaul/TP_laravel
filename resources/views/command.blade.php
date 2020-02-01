a666f4fd026724668df5f9cae2b256eb5f4a4c84
paul bernasconi <bernascpaul@gmail.com>
2020-02-01 15:30:21 +0000

    8_command

--- resources/views/command.blade.php
+++ resources/views/command.blade.php
@@ -1,5 +1,8 @@
 <h1 style="color: #4651f0;">Command</h1>
-<p>
+<p style="color: #545454;">
     Le fait d'utiliser une Commande comme modèle de concepton est que cela permet de paramétrer des méthodes avec différents paramêtres, de retarder ou de mettre en 
     file d'attente l'exécution d'une demande et de prendre en charge les opérations annulables.
+
+    A partir de cela, nous pouvons directement appeler dans la vue un controller en particulier ainsi qu'une fonction du controller en question afin d'éxecuter les fonctions
+
 </p>
